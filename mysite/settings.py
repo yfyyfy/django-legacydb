064825c35d0835c256a6ede700f6a4cd4f27f991
yfyyfy <32302435+yfyyfy@users.noreply.github.com>
2019-05-06 02:59:31 +0900

    Set up database

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'legacydb': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'legacydb.sqlite3'),
     }
 }
 
